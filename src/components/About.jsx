4f579c3fb79e296d80f0c266fc7f93c707e642e0
Amitagarwalrkt <107742055+Amitagarwalrkt@users.noreply.github.com>
2024-09-23 11:59:09 +0530

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div
+    name="About"
+    className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+  >
+    <div>
+      <h1 className="text-3xl font-bold mb-5">About</h1>
+      <p className='text-sm md:text-md text-justify'>
+        Hello, I'm Amit, a passionate Web developer with a keen eye for MERN
+        Stack . With a background in IT, I strive to create impactful and
+        visually stunning Software solutions that leave a lasting impression.
+      </p>
+      <br />
+      <h1 className="underline text-red-600 font-semibold text-xl">
+        Education & Training
+      </h1>
+     <div className="flex space-x-14 md:space-x-52  ">
+     <div >
+     <p className="text-sm md:text-md text-justify font-bold">
+           MCA <br />
+           Graphic Era Hill University <br />
+           BCA <br />                                                                                                   
+           Graphic Era Hill University <br />
+           Cloud Computing Jr. Analyst <br />                                                                                     
+           AWS
+      </p>
+      </div>
+
+      <div className="font-bold">
+      <p className="text-sm md:gap-x-6">
+          2024 -  present <br/> 
+          <br /> 2021 - 2024 <br/> 
+          <br /> 01/2024 - 06/2024 
+        </p>
+      </div>
+     </div>
+      <br />
+      <h1 className="underline text-red-600 font-semibold text-xl">
+        Skills & Expertise
+      </h1>
+      <p className="text-sm md:text-md text-justify font-bold ">
+        Proficient in HTML5, CSS, JavaScript, jQuery Mobile, figma, Bootstrap, Vite+React, PHP Programming , Angular, SQL, MySQL. <br />Experienced with 
+        Android Studio, VS Code, X Code, Sublime Text, PyCharm, Hybrid Mobile. <br />App Strong grasp of 
+        Front-End Development , Computer Science, Back-End Web Development, VCS, Maintaining Websites <br />
+        Excellent problem-solving skills Effective communicator and
+        collaborator
+      </p>
+      <br />
+      <h1 className="underline text-red-600 font-semibold text-xl">
+        Professional Experience
+      </h1>
+      <p className="text-sm md:text-md text-justify font-bold">
+        Freelancer, Freelancer.com, 2023 - Present During these work i develop my collabrating and technical skills. <br />Intern,
+        GAOTEK inc, 2024 here I develop my wordpress skills. <br />
+      </p >
+      <br />
+      <br />
+      <h1 className="underline text-red-600 font-semibold text-xl">
+        Achievements & Awards
+      </h1>
+      <p className="text-sm md:text-md text-justify font-bold">
+        <br />
+        Cloud Computing-Jr Analyst, AWS, 12/06/2024 
+      </p>
+      <br />
+      <br />
+      <h1 className="underline text-red-600 font-semibold text-xl">
+        Mission Statement
+      </h1>
+      <p className="text-sm md:text-md text-justify font-bold">
+        My mission is to leverage my skills and creativity to deliver
+        innovative React.js/node.js solutions that <br />exceed client expectations and
+        contribute positively to the digital landscape.
+        <br />I am committed to continuous learning and growth, always seeking new challenges and
+        opportunities to expand my horizons.
+      </p>
+    </div>
+  </div>
+  )
+}
+
+export default About
